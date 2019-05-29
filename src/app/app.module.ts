a82d9227648e54798ea192221afa02e27c86602c
Anderson Davanse <andervanse@gmail.com>
2019-05-28 22:42:47 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RedacoesAlunoComponent } from './components/redacoes-aluno/redacoes-aluno.component';
+
+
+const appRouter = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path:'home', component: HomeComponent },
+  { path:'cadastro', component: CadastroComponent },
+  { path:'contato', component: ContatoComponent },
+  { path:'sobre', component: SobreComponent },
+  { path:'entrar', component: LoginComponent },
+  { path:'redacoes-aluno', component: RedacoesAlunoComponent }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent,
+    LoginComponent,
+    CadastroComponent,
+    FooterComponent,
+    RedacoesAlunoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(appRouter, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/redacoes-aluno/redacoes-aluno.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redacoes-aluno',
+  templateUrl: './redacoes-aluno.component.html',
+  styleUrls: ['./redacoes-aluno.component.css']
+})
+export class RedacoesAlunoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
